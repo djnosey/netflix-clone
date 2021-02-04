35964ecf0d0bc9c8b1910ea0852077ea4a2161a8
steven payne <djnosey@hotmail.co.uk>
2021-02-04 13:45:02 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBY5IGMhrLWgqbaPxX-37M91QH-7tpICQ8",
+    authDomain: "netflix-clone-56f48.firebaseapp.com",
+    projectId: "netflix-clone-56f48",
+    storageBucket: "netflix-clone-56f48.appspot.com",
+    messagingSenderId: "967444906701",
+    appId: "1:967444906701:web:2a9c297072500ff00e9f84"
+  };
