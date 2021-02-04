d8cbccbab4fba424aa7127676b2125dd1c3b372d
steven payne <djnosey@hotmail.co.uk>
2021-02-04 13:59:53 +0100

    add homescreen component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import HomeScreen from "../src/compnents/HomeScreen";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>NETFLIX</h1>
+      <HomeScreen />
     </div>
   );
 }
