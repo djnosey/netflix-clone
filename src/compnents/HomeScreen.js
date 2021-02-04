26168e3b1b54fad268d0260706d581526849b489
steven payne <djnosey@hotmail.co.uk>
2021-02-04 14:43:03 +0100

    add navbar

--- src/compnents/HomeScreen.js
+++ src/compnents/HomeScreen.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import "./HomeScreen.css"
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
 
 function HomeScreen() {
-    return (
-        <div className ="homeScreen">
-            
-        </div>
-    )
+  return (
+    <div className="homeScreen">
+      <Nav />
+    </div>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
--- src/compnents/Nav.js
+++ src/compnents/Nav.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import "./Nav.css"
+import React from "react";
+import "./Nav.css";
 function Nav() {
-    return (
-        <div className="nav">
-            
-        </div>
-    )
+  return (
+    <div className="nav nav__black">
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt=""
+        />
+        <img
+          className="nav__avatar"
+          src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
